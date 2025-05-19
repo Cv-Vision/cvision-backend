b938fe556d4fda5686001fadde4ec964e7281a10
mmaglio <maximiliano.maglio@ing.austral.edu.ar>
2025-05-19 11:13:51 -0300

    added cors config (proxy integration) to lambda

--- lambda/generate_presigned_url/generate_presigned_url_handler.py
+++ lambda/generate_presigned_url/generate_presigned_url_handler.py
@@ -7,17 +7,19 @@ BUCKET_NAME = os.environ.get("BUCKET_NAME", "cvision-cv-bucket")
 
 def lambda_handler(event, context):
     try:
+        # Parse the incoming event to get the body
         body = json.loads(event.get('body', '{}'))
-        
+
         print("DEBUG EVENT:", event)
         print("DEBUG BODY:", body)
 
         job_id = body.get("job_id")
-        filenames = body.get("filenames")  # Esperamos un array
+        filenames = body.get("filenames")  # We expect an array of filenames
 
         if not job_id or not filenames or not isinstance(filenames, list):
             return {
                 "statusCode": 400,
+                "headers": cors_headers(),
                 "body": json.dumps({"error": "Se requiere job_id y un array de filenames"})
             }
 
@@ -43,6 +45,7 @@ def lambda_handler(event, context):
 
         return {
             "statusCode": 200,
+            "headers": cors_headers(),
             "body": json.dumps({
                 "job_id": job_id,
                 "presigned_urls": result
@@ -52,5 +55,14 @@ def lambda_handler(event, context):
     except Exception as e:
         return {
             "statusCode": 500,
+            "headers": cors_headers(),
             "body": json.dumps({"error": str(e)})
         }
+
+
+def cors_headers():
+    return {
+        "Access-Control-Allow-Origin": "http://localhost:5173",
+        "Access-Control-Allow-Headers": "Content-Type,Authorization",
+        "Access-Control-Allow-Methods": "OPTIONS,POST"
+    }
