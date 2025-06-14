3d132f496fb2b21a634656ef3f6cd13828fb7089
maxim <maximiliano.maglio@ing.austral.edu.ar>
2025-06-14 12:14:59 -0300

    feature: added new lambda: get-user-job-descriptions_handler.py

---
+++ lambda/get_user_job_descriptions/get-user-job-descriptions_handler.py
@@ -0,0 +1,41 @@
+import boto3
+import json
+import os
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['JOB_DESCRIPTIONS_TABLE'])
+
+def lambda_handler(event, context):
+    # Get user_id from the event
+    claims = event.get("requestContext", {}).get("authorizer", {}).get("claims", {})
+    user_id = claims.get("sub")
+
+    if not user_id:
+        return {
+            "statusCode": 401,
+            "body": json.dumps({"message": "Unauthorized - user_id not found"})
+        }
+
+    # Build the query to get all job descriptions for the user
+    try:
+        response = table.query(
+            IndexName='sk-index',
+            KeyConditionExpression="sk = :sk", # Using the secondary index for sk
+            ExpressionAttributeValues={
+                ":sk": f"USER#{user_id}"
+            }
+        )
+        items = response.get('Items', [])
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps(items),
+            "headers": {
+                "Content-Type": "application/json"
+            }
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)}),
+        }
