fb3877ee117791067d673faebb108bff5f0c8f75
mirkovrancic <mvrancic@mail.austral.edu.ar>
2025-06-29 20:00:46 -0300

    cambio headers

--- lambda/get_cv_download_url/get_cv_download_url_handler.py
+++ lambda/get_cv_download_url/get_cv_download_url_handler.py
@@ -10,11 +10,12 @@ JOB_POSTINGS_TABLE = os.environ["JOB_POSTINGS_TABLE"]
 UPLOADS_BUCKET = os.environ["UPLOADS_BUCKET"]
 
 CORS_HEADERS = {
-    "Access-Control-Allow-Origin": "http://localhost:3000",
-    "Access-Control-Allow-Headers": "Content-Type,Authorization",
-    "Access-Control-Allow-Methods": "OPTIONS,GET",
-    "Access-Control-Allow-Credentials": "true"
+    "access-control-allow-origin": "http://localhost:3000",
+    "access-control-allow-headers": "Content-Type,Authorization",
+    "access-control-allow-methods": "OPTIONS,GET",
+    "access-control-allow-credentials": "true"
 }
+
 
 
 def lambda_handler(event, context):
