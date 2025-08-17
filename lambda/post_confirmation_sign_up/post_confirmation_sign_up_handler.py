560905acfeea5401246b9a4bb91b217cd96c1914
maxim <maximiliano.maglio@ing.austral.edu.ar>
2025-08-17 20:32:26 -0300

    changing to new AWS Account: - fixed post_confirmation_sign_up_handler.py to create a user in the db

--- lambda/post_confirmation_sign_up/post_confirmation_sign_up_handler.py
+++ lambda/post_confirmation_sign_up/post_confirmation_sign_up_handler.py
@@ -1,10 +1,13 @@
 import boto3
+from db_handler import get_session
+from models import User
 
 def lambda_handler(event, context):
     user_pool_id = event['userPoolId']
     user_name = event['userName']
     user_attributes = event['request']['userAttributes']
     user_type = user_attributes.get('custom:userType')
+    user_id = user_attributes.get('sub')
 
     client = boto3.client('cognito-idp')
 
@@ -19,6 +22,7 @@ def lambda_handler(event, context):
             print(f'Error: Tipo de usuario desconocido: {user_type}')
             raise Exception('Tipo de usuario desconocido')
 
+    # Add user to the appropriate Cognito group
     if group_name:
         try:
             client.admin_add_user_to_group(
@@ -30,5 +34,26 @@ def lambda_handler(event, context):
         except Exception as e:
             print(f'Error agregando usuario al grupo: {e}')
             raise e
+
+    # Create user in the database
+    session = None
+    try:
+        session = get_session()
+        existing_user = session.query(User).filter(User.user_id == user_id).first()
+        if not existing_user:
+            new_user = User(
+                user_id=user_id,  # Cognito sub
+                name=user_name,  # Name
+                role=user_type.upper()
+            )
+            session.add(new_user)
+            session.commit()
+            print(f'Registro en DB creado para usuario {user_name}')
+    except Exception as e:
+        session.rollback()
+        print(f'Error creando usuario en DB: {e}')
+        raise e
+    finally:
+        session.close()
 
     return event
